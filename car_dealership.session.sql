0c05e22da740dc7739fd20f30386706131c23381
Armani978 <100504237+Armani978@users.noreply.github.com>
2022-05-01 17:39:44 -0700

    2nd commit

---
+++ car_dealership.session.sql
@@ -0,0 +1,22 @@
+-- ALTER TABLE car
+-- ADD color VARCHAR(50);
+
+-- ALTER TABLE car
+-- ALTER COLUMN make TYPE VARCHAR(50);
+
+-- UPDATE car
+-- SET make = 'Toyota' 
+-- WHERE model = 'Camry';
+
+-- ALTER TABLE sale_history
+-- ADD staff_id VARCHAR(50)
+
+-- ALTER TABLE sale_history
+-- ADD invoice VARCHAR(50);
+
+-- ALTER TABLE sale_history
+-- ALTER COLUMN sale_date TYPE VARCHAR(50);
+
+ALTER TABLE inventory
+DROP COLUMN sale_date
+
